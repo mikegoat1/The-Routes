a2dac124a6b1ae5b5a63866eec4107449e6e5ad8
Mikey J <mikeylashaun@icloud.com>
2021-07-12 23:14:56 -0700

    finished with Tutor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path"); 
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,20 +18,47 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+app.get("/exercise", (req,res) =>{
+    res.sendFile(path.join( __dirname, "./public/exercise.html"))
+})
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
 
 //create a route for creating a workout
 app.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err); 
-    })
-}); 
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
 
-app.put("/api/workouts/:id", (req, res)=>{
-    db.Workout.findOneAndUpdate(req.params.id, {$push: { exercises: req.body}}, {new: true})
+
+//Working on updating 
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+// Get route
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+            }
+        }
+    ])
     .then(dbWorkout => {
         res.json(dbWorkout); 
     })
@@ -38,6 +66,30 @@ app.put("/api/workouts/:id", (req, res)=>{
         res.json(err); 
     })
 }); 
+
+
+//Get route limit 
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+            }
+        }
+    ])
+    .limit(5)
+    .then(dbWorkout => {
+
+        res.json(dbWorkout); 
+    })
+
+    .catch(err => {
+        res.json(err); 
+    })
+}); 
+
+
 
 
 
@@ -49,5 +101,4 @@ app.put("/api/workouts/:id", (req, res)=>{
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
-  
+});
