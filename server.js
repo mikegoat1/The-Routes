66d7b2935200d6d54b892b09e66d3f40ccf2e3bf
Mikey J <mikeylashaun@icloud.com>
2021-07-12 20:01:47 -0700

    working on put/update route

--- server.js
+++ server.js
@@ -16,3 +16,38 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+//create a route for creating a workout
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err); 
+    })
+}); 
+
+app.put("/api/workouts/:id", (req, res)=>{
+    db.Workout.findOneAndUpdate(req.params.id, {$push: { exercises: req.body}}, {new: true})
+    .then(dbWorkout => {
+        res.json(dbWorkout); 
+    })
+    .catch(err => {
+        res.json(err); 
+    })
+}); 
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
