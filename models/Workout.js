bb0cddc5a3e70ca4afe589834cc00cf41011b2b5
Mikey J <mikeylashaun@icloud.com>
2021-07-12 19:07:28 -0700

    Inserting seed and fixing Workout DB

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema; 
 
 const WorkoutSchema = new Schema({
-    
+    day: {
+        type: Date,
+    },
+    exercises: [{
+        type: {
+            type:String,
+        },
+        name: {
+            type:String,
+
+        },
+        duration: {
+            type:Number, 
+
+        }, 
+        weight: {
+            type:Number, 
+
+        }, 
+        reps: {
+            type:Number, 
+
+        }, 
+        sets: {
+            type:Number,
+        },
+    }]
 }); 
 
 const Workout = mongoose.model("Workout", WorkoutSchema); 
--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
