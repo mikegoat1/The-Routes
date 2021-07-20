8c121d179b60e88f16b56f83dd3bb9c4260ce21a
Mikey J <mikeylashaun@icloud.com>
2021-07-19 21:41:59 -0700

    fixing readme

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path"); 
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,8 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/exercise", (req,res) =>{
-    res.sendFile(path.join( __dirname, "./public/exercise.html"))
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
 })
 
 app.get("/stats", (req, res) => {
@@ -40,6 +40,7 @@ app.post("/api/workouts", ({ body }, res) => {
 
 //Working on updating 
 app.put("/api/workouts/:id", (req, res) => {
+    //Grabing the parameter from the URL and adding it to the exercise object property 
     db.Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -50,7 +51,6 @@ app.put("/api/workouts/:id", (req, res) => {
 });
 
 // Get route
-
 app.get("/api/workouts", (req, res) => {
     db.Workout.aggregate([
         {
@@ -59,13 +59,13 @@ app.get("/api/workouts", (req, res) => {
             }
         }
     ])
-    .then(dbWorkout => {
-        res.json(dbWorkout); 
-    })
-    .catch(err => {
-        res.json(err); 
-    })
-}); 
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
 
 
 //Get route limit 
@@ -78,18 +78,28 @@ app.get("/api/workouts/range", (req, res) => {
             }
         }
     ])
-    .limit(5)
-    .then(dbWorkout => {
+        .limit(5)
+        .then(dbWorkout => {
 
-        res.json(dbWorkout); 
+            res.json(dbWorkout);
+        })
+
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+//Delete Route 
+
+app.delete('/api/workouts', ({ body }, res) => {
+    db.Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true); 
     })
-
     .catch(err => {
         res.json(err); 
-    })
-}); 
-
-
+    });
+});
 
 
 
